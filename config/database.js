38ddd799b04e275d8d7e47474b8e6f74b00e5665
EdASalazar <edasalazar123@gmail.com>
2023-02-03 16:20:37 -0800

    initial commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoos');
+
+mongoose.connect(process.env.DATABASE_URL);
+
+// shortcut va to mongoose.connection obj
+
+const db = mongoose.connection;
+
+db.on('connected', function(){
+    console.log('Connected to MongodB ${db.name} at ${db.host}:${db.port}');
+});
