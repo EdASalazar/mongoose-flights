1106345ea8db39e01979937b99c488eed90943a6
EdASalazar <edasalazar123@gmail.com>
2023-02-07 17:49:10 -0800

    Removed arrival from index

--- controllers/flights.js
+++ controllers/flights.js
@@ -15,9 +15,16 @@ function index(req, res) {
 
 function show(req, res) {
     Flight.findById(req.params.id, function(err, flight) {
-      res.render('flights/show', { title: 'Flight Detail', flight });
+        Ticket.find({flight: flight._id}, function(err, tickets) {  
+            console.log(tickets);
+      res.render('flights/show', { 
+        title: 'Flight Detail', 
+        flight , 
+        ticket 
     });
-  }
+    });
+  });
+}
 
 function newFlight(req, res) {
     res.render('flights/new');
--- models/flight.js
+++ models/flight.js
@@ -37,6 +37,10 @@ const flightSchema = new Schema({
         default: Date.now() + 365*24*60*60000
     },
     destinations: [destinationSchema],
+    tickets: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Ticket',
+    }],
 }, {
     timestamps: true
 });
