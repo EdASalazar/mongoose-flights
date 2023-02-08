fe892427ea2755a5b6cb8546d5975b7a5a443ef6
EdASalazar <edasalazar123@gmail.com>
2023-02-07 21:45:34 -0800

    mapping out the router and mounting the server.

---
+++ routes/tickets.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var ticketsCtrl = require('../controllers/tickets');
+
+
+// POST /flights/:id/tickets
+
+router.post('/flights/:id/tickets', ticketsCtrl.create);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('./config/database');
 var indexRouter = require('./routes/index');
 var flightsRouter = require('./routes/flights');
 var destinationsRouter = require('./routes/destinations');
+var ticketsRouter = require('./routes/tickets');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/flights', flightsRouter);
 // Mounting to root because not all routes
 // for destinations start with /destinations
 app.use('/', destinationsRouter);
+app.use('/', ticketsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
