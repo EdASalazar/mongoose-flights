992e913eddb952d0a3e2a17b3d91f7dc113e2fc7
EdASalazar <edasalazar123@gmail.com>
2023-02-09 10:40:51 -0800

    fixed a typo in tickets/create

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -8,7 +8,7 @@ module.exports = {
 };
 
 function create(req, res) {
-  req.body.flight = req.params.id;;
+  req.body.flight = req.params.id;
   Ticket.create(req.body, function(err, ticket) {
     res.redirect(`/flights/${req.params.id}`)
   });
